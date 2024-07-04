c538b8aa8041c9b459e0d9acd6ecf22589ece335
Juan Vidal <juanmvg2003@gmail.com>
2024-07-04 13:41:03 -0500

    String interpolation, ngIf, ngFor, ngSwitch, data binding, models

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
 
+// modelos productos
+import { Product } from './models/product.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-platzi';
+
+  name = 'Juan Vidal';
+  
+  // crud de personas
+  personas = ['Juan', 'Carlos', 'Julian'];
+  addPerson(){
+    this.personas.push('Nuevo item');
+  }
+
+  deletePerson(index:number){
+    this.personas.splice(index, 1);
+  }
+
+  // productos
+  produts:Product[] = [
+    {
+      id: '1',
+      image: 'https://images.pexels.com/photos/297933/pexels-photo-297933.jpeg',
+      title: 'Casmisa a Cuadros',
+      price: 70000,
+      description: 'Esta es una camisa a cuadros muy bonita', 
+    },
+    {
+      id: '2',
+      image: 'https://images.pexels.com/photos/2294342/pexels-photo-2294342.jpeg',
+      title: 'Casmisa Estampada',
+      price: 45000,
+      description: 'Esta es una camisa a estampada muy bonita', 
+    },
+    {
+      id: '3',
+      image: 'https://images.pexels.com/photos/4602025/pexels-photo-4602025.jpeg',
+      title: 'Conjunto de mujer',
+      price: 135000,
+      description: 'Este es un conjunto de mujer muy aestethic', 
+    }
+  ]
+
 }
