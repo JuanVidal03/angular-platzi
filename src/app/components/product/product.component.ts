bc064a65ddafa0a85e52c97474fc665d8740772e
Juan Vidal <juanmvg2003@gmail.com>
2024-07-09 13:07:20 -0500

    components cicle of live

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,15 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  SimpleChanges,
+  OnInit,
+  DoCheck,
+  OnDestroy
+} from '@angular/core';
+
 import { Product } from 'src/app/models/product.model';
 
 @Component({
@@ -6,7 +17,31 @@ import { Product } from 'src/app/models/product.model';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent {
+
+export class ProductComponent implements OnChanges, OnInit, DoCheck, OnDestroy {
+  // cada que se renderiza el contenido
+  constructor(){
+    console.log('1.Constructor');
+  }
+  // cada vez que hay cambios en el componente
+  ngOnChanges(changes: SimpleChanges){
+    console.log('2.ngOnChanges');
+    console.log(changes);
+  }
+  // se va a mostrar solamente la primera vez que se renderiza el componente
+  ngOnInit(){
+    console.log('3.ngOnInit');
+  }
+
+  // es casi lo mismo que ngOnChanges, este se puede personalizar mas
+  ngDoCheck(){
+    console.log('4.ngDoCheck');
+  }
+
+  //quita el componente de la vista
+  ngOnDestroy(){
+    console.log('5.ngOnDestroy');
+  }
 
   // recibir elementos del padre
   @Input() product!:Product;
