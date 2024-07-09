e8f87091e035f95bb08b7c0e9ee2cd6bacfa6e4c
Juan Vidal <juanmvg2003@gmail.com>
2024-07-09 12:16:55 -0500

    improve prodcut view, using inputs and outputs

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent {
+
+  // recibir elementos del padre
+  @Input() product!:Product;
+
+  // enviar datos al padre
+  @Output() onAddCart:EventEmitter<Product> = new EventEmitter();
+
+  addCart(){
+      this.onAddCart.emit(this.product);
+  }
+
+}
